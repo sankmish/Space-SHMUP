3099919b590948e83463afea5479b127d393dbca
SPM170 <SPM170@CILABS-114B-13.scilsnet.rutgers.edu>
2018-06-21 20:20:31 -0400

    Space SHMUP Spawning Enemies at Random
    Added SM_BoundsCheck to MainCamera and created Main script to instantiate enemies at random from a given array.

---
+++ Assets/__Scripts/Main.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+    static public Main S;
+
+    [Header("Set in Inspector")]
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f;
+    public float enemyDefaultPadding = 1.5f;
+
+    private SM_BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        S = this;
+        bndCheck = GetComponent<SM_BoundsCheck>();
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }
+
+    public void SpawnEnemy()
+    {
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        float enemyPadding = enemyDefaultPadding;
+        if (go.GetComponent<SM_BoundsCheck>() != null)
+        {
+            enemyPadding = enemyDefaultPadding;
+        }
+
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyPadding;
+        float xMax = bndCheck.camWidth - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyPadding;
+        go.transform.position = pos;
+
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }
+
+}
