9b638b2addb7e3945f16cb82e32114d660dc95e8
SPM170 <SPM170@CILABS-114B-13.scilsnet.rutgers.edu>
2018-06-21 20:09:21 -0400

    Space SHMUP Enemy Script
    Created and edited Enemy and BoundsCheck Scripts to delete Enemies when they go off screen.

--- Assets/__Scripts/SM_BoundsCheck.cs
+++ Assets/__Scripts/SM_BoundsCheck.cs
@@ -6,11 +6,15 @@ public class SM_BoundsCheck : MonoBehaviour
 {
     [Header("Set in Inspector")]
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
 
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
     void Awake()
     {
         camHeight = Camera.main.orthographicSize;
@@ -20,24 +24,41 @@ public class SM_BoundsCheck : MonoBehaviour
     void LateUpdate()
     {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
 
         if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
+            isOnScreen = false;
+            offRight = true;
         }
         if (pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
+            isOnScreen = false;
+            offLeft = true;
         }
         if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
+            isOnScreen = false;
+            offUp = true;
         }
         if (pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
+            isOnScreen = false;
+            offDown = true;
         }
-        transform.position = pos;
+
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
+        }   
     }
 
     void OnDrawGizmos()
